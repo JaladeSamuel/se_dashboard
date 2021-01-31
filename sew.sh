9e6f39de94930b4815892b93831dd54379262e2d
selimmch <selim.mch@gmail.com>
2021-01-31 21:06:02 +0100

    update sew.sh to run all files

--- sew.sh
+++ sew.sh
@@ -1,5 +1,10 @@
 #!bin/bash
 
+gnome-terminal -x "python sensors/anemo_sensor.py"
+gnome-terminal -x "python sensors/hum_sensor.py"
+gnome-terminal -x "python sensors/temp_ext_sensor.py"
+gnome-terminal -x "python sensors/indice_uv_sensor.py"
+
 cd dashboard-web
 
 gnome-terminal -e "google-chrome http://localhost:8000"
